b0b6c37304589d3dd5c9661bf9e803db88b6355b
chanhee <ichanhui578@gmail.com>
2024-10-20 14:10:00 +0900

    초기 세팅

---
+++ week04/my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+}
+
+export default App;
